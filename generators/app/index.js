92db976371169fdf14516adf56065bc7be6c3723
r1ck5 <ricardosasaki1@gmail.com>
2022-07-22 21:45:23 +0300

    tests

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,10 +4,21 @@ module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
 
+        // This method adds support for a '--babel' flag
         this.option('babel');
+
+        this.helperMethod = function() {
+            console.log('won\'t be called automatically.');
+        }
     }
+
+    _private_method() {
+        console.log('private hey...');
+    }
+
 
     initialConfig() {
         this.log('...');
     }
+
 };
