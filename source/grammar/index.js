5b952296a89b20059554cc06ab7868c18b27c4e7
r1ck5 <ricardosasaki1@gmail.com>
2022-08-01 22:01:08 +0300

    .

--- source/grammar/index.js
+++ source/grammar/index.js
@@ -30,52 +30,52 @@ const lexer = new CSharpLexer(chars);
 const tokens = new antlr4.CommonTokenStream(lexer);
 const parser = new CSharpParser(tokens);
 parser.buildParseTrees = true;
-var tree = parser.method_declaration();
+// var tree = parser.method_declaration();
 
-class Visitor  extends CSharpParserVisitor {
-    visitClass_definition(ctx) {
-        // super.visitClass_definition(ctx);
+// class Visitor  extends CSharpParserVisitor {
+//     visitClass_definition(ctx) {
+//         // super.visitClass_definition(ctx);
 
-        if (!ctx) {
-        return;
-        }
+//         if (!ctx) {
+//         return;
+//         }
 
-        if (ctx.children) {
-        var childrens = ctx.children.map(child => {
-            // if (child.children && child.children.length != 0) {
-            //   return child.accept(this);
-            // } else {
-            return child.getText();
-            // }
-        });
-        console.log(childrens);
-        return childrens;
-        }
+//         if (ctx.children) {
+//         var childrens = ctx.children.map(child => {
+//             // if (child.children && child.children.length != 0) {
+//             //   return child.accept(this);
+//             // } else {
+//             return child.getText();
+//             // }
+//         });
+//         console.log(childrens);
+//         return childrens;
+//         }
 
+//     }
+// }
+
+// tree.accept(new Visitor());
+
+
+class MyGrammarListener extends CSharpParserListener {
+    constructor() {
+        super();
+    }
+   
+    enterKey(ctx) {}
+    exitKey(ctx) {}
+    enterValue(ctx) {}
+    exitValue(ctx) {}
+}
+
+class KeyPrinter extends MyGrammarListener {
+    // override default listener behavior
+    exitKey(ctx) {
+        console.log("Oh, a key!");
     }
 }
 
-tree.accept(new Visitor());
-
-
-// class MyGrammarListener extends CSharpParserListener {
-//     constructor() {
-//         super();
-//     }
-   
-//     enterKey(ctx) {}
-//     exitKey(ctx) {}
-//     enterValue(ctx) {}
-//     exitValue(ctx) {}
-// }
-
-// class KeyPrinter extends MyGrammarListener {
-//     // override default listener behavior
-//     exitKey(ctx) {
-//         console.log("Oh, a key!");
-//     }
-// }
-
-// var tree = parser.class_definition() // assumes grammar "MyGrammar" has rule "MyStartRule"
-// const printer = new KeyPrinter();
-// antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, tree);
+var tree = parser.using_directives(); // assumes grammar "MyGrammar" has rule "MyStartRule"
+const printer = new KeyPrinter();
+antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, tree);
